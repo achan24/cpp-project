98f84226d9a4a0ea9ce37f8e03dced67a58a54fb
achan24 <58598070+achan24@users.noreply.github.com>
2025-03-10 17:34:42 +0000

    implement admin interface for accounts and cart

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import UserProfile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'User Profile'
+    fk_name = 'user'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_county')
+    list_select_related = ('userprofile',)
+    
+    def get_county(self, instance):
+        try:
+            return instance.userprofile.county
+        except UserProfile.DoesNotExist:
+            return ''
+    
+    get_county.short_description = 'County'
+    
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return []
+        return super().get_inline_instances(request, obj)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import CartItem
 
-# Register your models here.
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['user', 'get_username', 'plant', 'quantity', 'session_id', 'date_added', 'get_total_price']
+    list_filter = ['date_added', 'user']
+    search_fields = ['user__username', 'plant__name', 'session_id']
+    date_hierarchy = 'date_added'
+    ordering = ['-date_added']
+    
+    def get_username(self, obj):
+        if obj.user:
+            return obj.user.username
+        return 'Anonymous'
+    get_username.short_description = 'Username'
+    
+    def get_total_price(self, obj):
+        return obj.total_price()
+    get_total_price.short_description = 'Total Price'
