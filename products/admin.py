e079edf45cfd8cbed15a91431842f0323b6a4d3c
achan24 <58598070+achan24@users.noreply.github.com>
2025-03-10 17:26:14 +0000

    implement custom admin interface for products app

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Plant
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+@admin.register(Plant)
+class PlantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'stock', 'available', 'difficulty', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated', 'category', 'difficulty']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'name': ('name',)}
+    search_fields = ['name', 'description']
+    date_hierarchy = 'created'
+    ordering = ['name', 'price']
